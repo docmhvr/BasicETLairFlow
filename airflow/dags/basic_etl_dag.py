382c4ccff71c700c786e30284d4cee75e259a4df
doc_mhvr <38325828+docmhvr@users.noreply.github.com>
2024-06-23 17:13:23 +0000

    Created basic dag and imported libraries

---
+++ airflow/dags/basic_etl_dag.py
@@ -0,0 +1,6 @@
+''' Basic ETL DAG'''
+from datetime import datetime, date
+import pandas as pd
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow import DAG
